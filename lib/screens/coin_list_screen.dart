74e4342a91866ff4dfbf294ff44a5fd7877ca60c
mehdishamekhi <mehdi.shamekhi22@gmail.com>
2023-05-23 05:43:02 +0330

    afd

--- lib/screens/coin_list_screen.dart
+++ lib/screens/coin_list_screen.dart
@@ -16,45 +16,46 @@ class _CoinListScreenState extends State<CoinListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xffADEFD1),
-                Color(0xff00203F),
-              ],
-            ),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xffADEFD1),
+              Color(0xff00203F),
+            ],
           ),
-          child: BlocBuilder<CryptoListBloc, CryptoListState>(
-            builder: (context, state) {
-              if (state is CryptoListLoadingState) {
-                return SpinKitChasingDots();
-              } else {
-                return SafeArea(
-                  child: ListView.builder(
-                    itemCount: 30,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: const Color(0xffADEFD1),
-                            width: 2,
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.transparent,
+        ),
+        child: BlocBuilder<CryptoListBloc, CryptoListState>(
+          builder: (context, state) {
+            if (state is CryptoListLoadingState) {
+              return SpinKitChasingDots();
+            } else {
+              return SafeArea(
+                child: ListView.builder(
+                  itemCount: 30,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color(0xffADEFD1),
+                          width: 2,
                         ),
-                        height: 100,
-                        child: const ListTile(),
-                      );
-                    },
-                  ),
-                );
-              }
-            },
-          )),
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.transparent,
+                      ),
+                      height: 100,
+                      child: const ListTile(),
+                    );
+                  },
+                ),
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
